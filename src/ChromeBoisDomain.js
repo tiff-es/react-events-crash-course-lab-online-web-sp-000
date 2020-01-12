22abfd11e42f39928c4e6e284852314270a51b4c
boostinwrx <tony.calablava@gmail.com>
2020-01-12 02:57:24 -0500

    Done.

--- src/ChromeBoisDomain.js
+++ src/ChromeBoisDomain.js
@@ -3,35 +3,59 @@ import { drawChromeBoiAtCoords, toggleCycling, resize } from './canvasHelpers.js
 
 
 export default class ChromeBoisDomain extends Component {
-  
-  handleMouseMove = (event) => {
-    /* TODO: This method should capture the `x` and `y` coordinates of the mouse
-     * from the event and use them to invoke the `drawChromeBoiAtCoords`
-     * function that has been provided and is already imported
-     * (`drawChromeBoiAtCoords` expects two arguments, an x and a y coordinate)
+
+    handleMouseMove = (event) => {
+        let x = event.clientX
+        let y = event.clientY
+        drawChromeBoiAtCoords(x,y)
+        // /XXXTODO: This method should capture the `x` and `y` coordinates of the mouse
+        //  * from the event and use them to invoke the `drawChromeBoiAtCoords`
+        //  * function that has been provided and is already imported
+        //  * (`drawChromeBoiAtCoords` expects two arguments, an x and a y coordinate)
+        //
+
+        // console.log({x,y})
+    }
+
+    /* XXXXTODO: Create an event handler which, when fired, invokes the provided
+     * `toggleCycling` function with no arguments. Don't forget the click event
+     * listener that should fire it!
      */
-  }
-  
-  /* TODO: Create an event handler which, when fired, invokes the provided
-   * `toggleCycling` function with no arguments. Don't forget the click event
-   * listener that should fire it!
-   */
-   
-  /* TODO: Add an event listener to the `<canvas>` element to capture when a key
-  /* is pressed. When a key is pressed, an event handler should invoke the the
-  /* provided `resize` function with a single argument of either '+' or '-'
-  /* if the key pressed was 'a', then it should call `resize` with '+'
-  /* if the key pressed was 's', then it should call `resize` with '-' 
-   */
-  
-  render() {
-    return (
-      <canvas 
-        onMouseMove={this.handleMouseMove}
-        width='900'
-        height='600'
-        tabIndex="0">
-      </canvas>
-    )
-  }
+    handleKeyDown = (event) => {
+        // let key = event.key
+        // switch (key) {
+        //     case ('a'):
+        //         console.log(key)
+        console.log(event)
+        let k = event.key
+        switch (k) {
+            case('a'):
+                resize('+')
+            case('s'):
+                resize('-')
+            default:
+                console.warn(':( Wrong key')
+        }
+
+    }
+
+    /* TODO: Add an event listener to the `<canvas>` element to capture when a key
+    /* is pressed. When a key is pressed, an event handler should invoke the the
+    /* provided `resize` function with a single argument of either '+' or '-'
+    /* if the key pressed was 'a', then it should call `resize` with '+'
+    /* if the key pressed was 's', then it should call `resize` with '-'
+     */
+
+    render() {
+        return (
+            <canvas
+                onKeyDown={this.handleKeyDown}
+                onClick={() => {toggleCycling()}}
+                onMouseMove={this.handleMouseMove}
+                width='900'
+                height='600'
+                tabIndex="0">
+            </canvas>
+        )
+    }
 }
